0df576c1c0abb4c8da770d84fc2ab471f927c84a
devlucas <lussivbsilva1@gmail.com>
2023-11-24 13:54:50 -0300

    Aprimorando Código

--- script.js
+++ script.js
@@ -1,21 +1,16 @@
 const readlineSync = require('readline-sync');
 
 const numInt = readlineSync.questionInt('Digite o valor desejado: ');
-const numbers = [] 
+let nums = 0
 
 const getNumbers = (valInt) =>{
     for(i = 0; i < valInt; i++){    
         if(i < valInt && ( i % 3 === 0 || i % 5 == 0)){  
-            numbers.push(i)
+            nums += i
         }
     }
+    console.log(nums)
 }
 
 getNumbers(numInt)
-
-const sum = numbers.reduce( (acc, curr)  => {
-    return acc + curr
-  }, 0)
-
-console.log(sum)
 
